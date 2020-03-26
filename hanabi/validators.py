0fb615463480bc99c48145a2307759ab3acc6324
Elijah <elijah498@gmail.com>
2020-03-26 12:06:26 -0400

    Name must be comprised of letters or numbers.  Refactored create-lobby name so errors only appeared for the corresponding boxes

--- hanabi/validators.py
+++ hanabi/validators.py
@@ -13,3 +13,28 @@ class ValidAccessToken:
         token = field.data
         if token not in lobbies:
             raise wtforms.ValidationError(self.message)
+
+
+def has_valid_asciis(word):
+    for char in word:
+        asc_val = ord(char)
+        if not (
+            48 <= asc_val <= 57 or 65 <= asc_val <= 90 or 97 <= asc_val <= 122
+        ):
+            return False
+    return True
+
+
+class ValidAsciiValues:
+    def __init__(self, message=None):
+        if not message:
+            message = (
+                "Requested username contains invalid characters "
+                "(must be letters or numbers)."
+            )
+        self.message = message
+
+    def __call__(self, form, field):
+        requested_player_id = field.data
+        if not has_valid_asciis(requested_player_id):
+            raise wtforms.ValidationError(self.message)
