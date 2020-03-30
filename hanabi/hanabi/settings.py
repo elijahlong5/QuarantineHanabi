289ecc00b143f3715d46a1fcbfaa76f29c53de7b
Elijah <elijah498@gmail.com>
2020-03-30 12:23:13 -0400

    add first view

--- hanabi/hanabi/settings.py
+++ hanabi/hanabi/settings.py
@@ -94,7 +94,7 @@ ROOT_URLCONF = "hanabi.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
