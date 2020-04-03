6d98265dd1ea9f9bc1539c6ccd6b03ed7b73955c
Elijah <elijah498@gmail.com>
2020-04-03 15:14:16 -0400

    Add card display for client

--- hanabi/game/static/game/scripts/game-in-session.js
+++ hanabi/game/static/game/scripts/game-in-session.js
@@ -27,92 +27,113 @@ async function fetchPostPlayerMove(playCardDict, accessToken, playerId) {
     return await response.json();
 }
 
+function numberToColor(num) {
+    let color = "";
+    switch (num) {
+        case 1:
+            color = "Blue";
+            break;
+        case 2:
+            color = "Green";
+            break;
+        case 3:
+            color = "Rainbow";
+            break;
+        case 4:
+            color = "Red";
+            break;
+        case 5:
+            color = "White";
+            break;
+        case 6:
+            color = "Yellow";
+            break;
+        default:
+            color = "Yellow";
+    }
+    return color
+}
+
+function initiateDisplay(gameCode, playerName) {
+    pollGameState(gameCode, playerName);
+}
+
+function pollGameState(gameCode, playerName) {
+    fetchGameState(gameCode, playerName)
+        .then(populateDisplay.bind(this))
+        .then( function() {
+           setTimeout(
+             pollGameState.bind(this, gameCode, playerName),
+               GAME_UPDATE_INTERVAL_MILLIS
+           );
+        });
+}
 
 
-function initiateDisplay(gameCode) {
+function populateDisplay( gameState ) {
     document.getElementById("hands").innerHTML = "";
     document.getElementById("deck").innerHTML = "";
     document.getElementById("bombs").innerHTML = "";
     document.getElementById("piles").innerHTML = "";
-    fetchGameState(gameCode)
-        .then( function (gameState) {
-            console.log(gameState);
-            let baseLink = "/static/hanabi_deck/";
-            let cardBackLink = baseLink + "card_back.png";
-            // Display players' hands
-            for (let curPId in gameState['players']) {
-                // make player div
-                let playerDiv = document.createElement("div");
-                playerDiv.id = curPId;
-                $("#hands").append(playerDiv);
 
-                let playerHeader = document.createElement("h3");
-                playerHeader.innerText = curPId;
-                if (curPId === gameState['whose-turn']){
-                    playerHeader.classList.add("highlight");
-                }
-                playerDiv.appendChild(playerHeader);
+    let remainingBombs = gameState["remaining_bombs"];
+    let remainingCards = gameState["remaining_cards"];
+    let players = gameState["players"];
 
-                let cardsDiv = document.createElement("div");
-                cardsDiv.classList.add("cards-container");
-                playerDiv.appendChild(cardsDiv);
+    let baseLink = "/static/hanabi_deck/";
+    let cardBackLink = baseLink + "card_back.png";
 
-                for (let card in gameState['players'][curPId]) {
-                    let curImg = document.createElement("img");
-                    let color = gameState['players'][curPId][card][0];
-                    let rank = gameState['players'][curPId][card][1];
-                    let cardId = gameState['players'][curPId][card][2];
-                    let link = (curPId === playerId) ? (cardBackLink) : (baseLink+color+"_"+rank+".png");
-                    curImg.src = link;
-                    cardsDiv.appendChild(curImg);
-                    curImg.addEventListener("click", function() {
-                        event.preventDefault();
+    // Display players' hands
+    for (let p in players) {
+        let curPlayerName = players[p]["name"];
+        let playerCards = players[p]["cards"];
+        let playerOrder = players[p]["order"];
 
-                        // Todo: for now all own-hand card moves are "plays"
-                        let move = (curPId === playerId) ? "play" : "hint";
-                        let cardIndex = (curPId === playerId) ? card : cardId;
-                        let hintType = (curPId === playerId) ? null : "rank";
+        // make player div
+        let playerDiv = document.createElement("div");
+        playerDiv.id = curPlayerName;
+        $("#hands").append(playerDiv);
 
-                        let moveDict = {
-                            "move": move, // = play discard or hint
-                            "card-index": cardIndex, // REQUIRED: 0-3 index for what card is selected
-                            "player-id": curPId, // REQUIRED: = key if play or discard. otherwise, who they are hinting to
-                            "hint-type": hintType, // REQUIRED IF HINT: either rank or color.
-                        };
+        let playerHeader = document.createElement("h3");
+        playerHeader.innerText = curPlayerName;
+        playerDiv.appendChild(playerHeader);
 
-                        console.log("move dict is:");
-                        console.log(moveDict);
+        let cardsDiv = document.createElement("div");
+        cardsDiv.classList.add("cards-container");
+        playerDiv.appendChild(cardsDiv);
 
-                        fetchPostPlayerMove(moveDict, accessToken, playerId).then(r => {
-                            console.log("move response received" + r);
-                            if (r !== {"status": "not your turn"}) {
-                                initiateDisplay(accessToken, playerId);
-                            }
-                        })
-                    });
-                }
+
+        for (let card in playerCards) {
+            let link = cardBackLink;
+            let cardOrder = card;
+            if ("color" in playerCards[card]) {
+                let color = numberToColor(playerCards[card]["color"]);
+                let rank = playerCards[card]["number"];
+                link = baseLink+color+"_"+rank+".png";
             }
-            // Display deck.
-            let deckImg = document.createElement("img");
-            deckImg.classList.add("card");
-            deckImg.src = cardBackLink;
-            $("#deck").append(deckImg);
-            let deckCount = document.createElement("h3");
-            deckCount.innerHTML = "Remaining:</br>" + gameState['cards-in-deck'];
-            deckCount.classList.add("centered");
-            $("#deck").append(deckCount);
 
-            // Display Bomb Count.
-            let bombCountTitle = document.createElement("h3");
-            bombCountTitle.innerText = "Bombs remaining: " + gameState['bomb-count'];
-            $("#bombs").append(bombCountTitle);
+            let curImg = document.createElement("img");
+            curImg.src = link;
+            cardsDiv.appendChild(curImg);
+            curImg.addEventListener("click", function() {
+                event.preventDefault();
+                console.log("Card " + cardOrder + " was clicked!");
+            });
+        }
+    }
 
-            // Display pile thing
-            let pilesDisplay = document.createElement("div");
-            $("#piles").append(pilesDisplay);
-            pilesDisplay.innerHTML = "";
-            for (let color in gameState['piles']) {
-                pilesDisplay.innerHTML += color+ ": " + gameState['piles'][color] + "</br>";
-            }
-        });
+    // Display deck.
+    let deckImg = document.createElement("img");
+    deckImg.classList.add("card");
+    deckImg.src = cardBackLink;
+    $("#deck").append(deckImg);
+    let deckCount = document.createElement("h3");
+    deckCount.innerHTML = "Remaining:</br>" + remainingCards;
+    deckCount.classList.add("centered");
+    $("#deck").append(deckCount);
+
+    // Display Bomb Count.
+    let bombCountTitle = document.createElement("h3");
+    bombCountTitle.innerText = "Bombs remaining: " + remainingBombs;
+    $("#bombs").append(bombCountTitle);
 }
