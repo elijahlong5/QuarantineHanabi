0d3614176561f41d57348c941a6d83ff35e3c784
Elijah <elijah498@gmail.com>
2020-04-15 13:30:21 -0400

    hint feature added.

--- hanabi/game/static/game/scripts/game-in-session.js
+++ hanabi/game/static/game/scripts/game-in-session.js
@@ -4,13 +4,28 @@ const ID_BOMBS = 'bombs-remaining';
 const ID_CARDS_REMAINING = 'cards-remaining';
 const CARD_ID_SUFFIX = "s-cards"; // playerName + suffix
 
+// Card link strings
 const LINK_BASE_CARD = "/static/hanabi_deck/";
 const LINK_CARD_BACK = LINK_BASE_CARD + "card_back.png";
 
+// css classes
+const UNSELECTED_CARD_CLASS = "unselected-card";
+const SELECTED_CARD_CLASS = "selected-card";
+
 let prevGameState = {};
 
+// Variables set from the template
 const GAME_CODE = window.gameCode;
 const PLAYER_NAME = window.playerName;
+
+// Hint button ids
+const ID_BUTTON_NUMBER_HINT = "hint-number-button-id";
+const ID_BUTTON_COLOR_HINT = "hint-color-button-id";
+
+// Card image attributes
+const ATTR_NAME = "data-player-name";
+const ATTR_COLOR = "data-card-color";
+const ATTR_NUMBER = "data-card-number";
 
 async function fetchGameState() {
     return fetch("/api/games/" + GAME_CODE + "/?as_player=" + PLAYER_NAME)
@@ -69,6 +84,8 @@ function pollGameState() {
 }
 
 function populateDisplay( gameState ) {
+    // Function when page is loaded.  Displays bombs, hands, deck etc.
+
     // game state elements
     let remainingBombs = gameState["remaining_bombs"];
     let remainingCards = gameState["remaining_cards"];
@@ -122,7 +139,11 @@ function populateDisplay( gameState ) {
     prevGameState = gameState;
 }
 
-function manageHandDisplay(cardsDiv, playerCards) {
+function manageHandDisplay(cardsDiv, currentPlayerDict) {
+    // Displays images for the cards in  each players hand
+    // Sets card attributes
+    let playerCards = currentPlayerDict["cards"];
+    let curPlayerName = currentPlayerDict["name"];
     let activeIdList = [];
 
     let childCardIds = [];
@@ -136,18 +157,30 @@ function manageHandDisplay(cardsDiv, playerCards) {
 
     for (let card in playerCards) {
         let cardOrder = card;
-        let cardId = playerCards[card]["id"];
-
+        let cardId = playerCards[card]["id"]
         activeIdList.push(cardId);
 
         if (!(childCardIds.indexOf(cardId) >= 0) && cardId !== undefined) {
             let curImg = document.createElement("img");
             curImg.id = cardId;
             curImg.src = genCardLink(playerCards[card]);
+            curImg.classList.add(UNSELECTED_CARD_CLASS);
+
+            // Set card attributes into the image.
+            curImg.setAttribute(ATTR_NAME, curPlayerName);
+            if ('color' in playerCards[card]) { // Assuming if color then number.
+                curImg.setAttribute(ATTR_COLOR, playerCards[card]["color"]);
+                curImg.setAttribute(ATTR_NUMBER, playerCards[card]["number"]);
+            }
+
             cardsDiv.appendChild(curImg);
             curImg.addEventListener("click", function () {
                 event.preventDefault();
-                handlePlayerMove(cardId);
+                if (genCardLink(playerCards[card]) === LINK_CARD_BACK) {
+                    handlePlayerMove(cardId);
+                } else {
+                    handleOnClickCreateHintOptions(cardId);
+                }
             });
         }
     }
@@ -162,9 +195,102 @@ function manageHandDisplay(cardsDiv, playerCards) {
     });
 }
 
-function genCardLink(card) {
-    let link = LINK_CARD_BACK;
+function handleOnClickCreateHintOptions(cardId) {
+    let choiceDiv = document.createElement("div");
+    const HINT_CHOICES_ID = "hint-choices";
+    choiceDiv.id = HINT_CHOICES_ID;
 
+    let bodyElem = document.getElementsByTagName("body")[0];
+    let cardImgElem = document.getElementById(cardId);
+
+    // Manage the element classes
+    cardImgElem.classList.remove(UNSELECTED_CARD_CLASS);
+    cardImgElem.classList.add(SELECTED_CARD_CLASS);
+
+    let cardHandDiv = cardImgElem.parentElement.parentElement;
+
+    let cardPosition = $("#"+cardHandDiv.id).position();
+
+    // Create the two buttons
+    let buttonHintNumber = document.createElement('button');
+    buttonHintNumber.innerText = "Number";
+    buttonHintNumber.id = ID_BUTTON_NUMBER_HINT;
+    let buttonHintColor = document.createElement('button');
+    buttonHintColor.innerText = "Color";
+    buttonHintColor.id = ID_BUTTON_COLOR_HINT;
+
+    buttonHintNumber.addEventListener("click", function() {
+        event.preventDefault();
+        fetchPostPlayerHint(cardId, "number", cardHandDiv.id)
+            .then(r => {
+                console.log(r);
+                clearHintOptions();
+            })
+    });
+    buttonHintColor.addEventListener("click", function() {
+        event.preventDefault();
+        fetchPostPlayerHint(cardId, "color", cardHandDiv.id)
+            .then(r => {
+                console.log(r);
+                clearHintOptions();
+            })
+    });
+
+    // Append the elements and position the button div
+    choiceDiv.appendChild(buttonHintNumber);
+    choiceDiv.appendChild(buttonHintColor);
+
+    $("#"+HINT_CHOICES_ID).remove();
+    bodyElem.appendChild(choiceDiv);
+
+    $("#"+HINT_CHOICES_ID).css({
+        position: "absolute",
+        top: cardPosition.bottom + "px",
+        left: (cardPosition.left) + "px",
+        width: "150%",
+    });
+}
+
+async function fetchPostPlayerHint( cardId, hintType, targetPlayerName ) {
+    let cardElem = document.getElementById(cardId);
+    let cardColor = cardElem.getAttribute(ATTR_COLOR);
+    let cardNumber = cardElem.getAttribute(ATTR_NUMBER);
+    let cardAttr = cardNumber;
+    if (hintType === "number"){
+        cardAttr = cardNumber;
+    } else {
+        cardAttr = cardColor;
+    }
+
+    let data = {
+        "action_type": "HINT",
+        "player_name": PLAYER_NAME,
+        'hint_action': {
+            [hintType]: cardAttr,
+            "target_player_name": targetPlayerName,
+        }
+    };
+
+    url = "/api/games/" + GAME_CODE + "/actions/";
+    const response = await fetch(url, {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrer: 'no-referrer',
+        body: JSON.stringify(data),
+    });
+
+    return await response.json();
+}
+
+function genCardLink(card) {
+    // Creates the image source link for the card.
+    let link = LINK_CARD_BACK;
     if ("color" in card) {
         let color = card["color"];
         let rank = card["number"];
@@ -192,6 +318,21 @@ function updateDisplay(gameState) {
 
         let cardsDivId = curPlayerName + CARD_ID_SUFFIX;
         let cardsDiv = document.getElementById(cardsDivId);
-        manageHandDisplay(cardsDiv, playerCards);
+        manageHandDisplay(cardsDiv, curP);
     }
+}
+
+function clearHintOptions() {
+    // Removes number and color buttons.  Resets all cards to 'unselected'
+
+    for (let c of document.getElementsByClassName(SELECTED_CARD_CLASS)){
+        c.classList.remove(SELECTED_CARD_CLASS);
+        c.classList.add(UNSELECTED_CARD_CLASS);
+    }
+
+    let bColor = document.getElementById(ID_BUTTON_COLOR_HINT);
+    let bNumber = document.getElementById(ID_BUTTON_NUMBER_HINT);
+    console.log('here');
+    bColor.parentNode.removeChild(bColor);
+    bNumber.parentNode.removeChild(bNumber);
 }
